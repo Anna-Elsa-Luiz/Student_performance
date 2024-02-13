7e46968520a60398694b50213e9420ffdccaa53b
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-02-13 16:40:07 +0530

    package created

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup 
+from typing import List
+
+
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path: str) -> List[str]:
+
+     '''
+     This function will return the list of requirements
+     
+     '''
+     requirements = []
+     with open(file_path) as file_obj:
+          requirements=file_obj.readlines()
+          [req.replace('\n',' ')for req in requirements]
+
+          
+          if HYPHEN_E_DOT in requirements:
+               requirements.remove(HYPHEN_E_DOT)
+
+
+setup(
+
+    name= 'mlproject' , 
+    version ='0.0.1',
+    author = 'Anna_Elsa_Luiz',
+    author_email ='annaelsaluiz@gmail.com',
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
